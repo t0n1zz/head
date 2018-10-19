8374f9333df50268e261774345eef87122c66dd7
Tony <tony762843@yahoo.co.id>
2018-10-19 22:11:59 +0700

    adding jwt auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('shop_id');
+            $table->integer('is_active')->default(0);
+            $table->integer('type')->default(0);
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('mobile')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
